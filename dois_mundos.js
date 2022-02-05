5314bad386ff42c76947a571fe679c5bc03c2da7
Mahiny <93958141+Mahiny@users.noreply.github.com>
2022-02-04 21:41:43 -0300

    Add files via upload

---
+++ dois_mundos.js
@@ -0,0 +1,31 @@
+<meta charset="UTF-8">
+<script>
+    document.write("Estou escrevendo do mundo JavaScript no mundo HTML");
+</script>
+
+<br>
+
+<meta charset="UTF-8">
+Estou escrevendo do mundo JavaScript no mundo HTML
+
+<br>
+
+Aqui também é mundo HTML, porque esta fora da TAG script
+
+<br>
+
+<meta charset="UTF-8">
+<script>
+    document.write(10 + 20);
+</script>
+ <br>
+
+ <meta charset="UTF-8">
+<script>
+    document.write("<h1>Seja bem-vindo</h1>");
+</script>
+
+<br>
+
+<meta charset="UTF-8">
+<h1>Seja bem-vindo</h1>
